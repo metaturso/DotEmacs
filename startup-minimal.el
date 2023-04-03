fa21d3fd606b35615d97c108ed1c99d97510b39d
Andrea <metaturso@users.noreply.github.com>
2023-04-04 00:18:37 +0100

    Disabled echo area messages.

--- startup-minimal.el
+++ startup-minimal.el
@@ -7,18 +7,30 @@
 (setq vc-handled-backends nil)
 
 ;; Disable audible or visible bell ESC or other events.
-(setq visible-bell 1)
-(setq ring-bell-function 'ignore)
+(setq visible-bell 1
+      ring-bell-function 'ignore)
 
-;; Customise Emacs variables.
-(setq inhibit-startup-message t
-      initial-scratch-message nil
-      default-directory "~/")
+;; Default Find-File directory
+(setq default-directory "~/")
 
 ;; Preload the prompts so that M-p can be used instead of typing.
 (setq yes-or-no-p-history '("yes" "no"))
 
-;; Customise frame
+;;; Inhibit all startup messages.
+;;
+;; Disabling the echo area message needs atypical handling.  Read
+;; https://yrh.dev/blog/rant-obfuscation-in-emacs/ for a good story.
+;;
+;; Replace RMS's echo area message function with one that does nothing
+;; instead.
+(advice-add 'display-startup-echo-area-message :override #'ignore)
+
+;; Disable startup buffer and scratch message
+(setq inhibit-startup-message t
+      initial-scratch-message nil)
+
+;;; Frame customisation
+;;
 (blink-cursor-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
